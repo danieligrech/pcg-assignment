2fa1aa2e9a749c35e7ef78100511f04e54e5d899
danieligrech <danieligrech@gmail.com>
2023-01-13 15:35:54 +0100

    Level Three Part 1/5
    Finished the first video of Terrain Generation

---
+++ PCG Home Assignment/Assets/Scripts/Terrain/LoadHeightMap.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LoadHeightMap : MonoBehaviour
+{
+    private Terrain terrain;
+    private TerrainData terrainData;
+
+    [SerializeField]
+    private Texture2D heightMapImage;
+
+    [SerializeField]
+    private Vector3 heightMapScale = new Vector3(1, 1, 1);
+
+    [SerializeField]
+    private bool loadHeightMap = true;
+
+    [SerializeField]
+    private bool flattenTerrainOnExit = true;
+
+    [SerializeField]
+    private bool loadHeightMapInEditMode = false;
+
+    [SerializeField]
+    private bool flattenTerrainInEdit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //terrain = this.GetComponent<Terrain>();
+        //terrainData = Terrain.activeTerrain.terrainData;
+
+        if(loadHeightMap){
+            LoadHeightMapImage();
+        } 
+    }
+
+    void LoadHeightMapImage(){
+        terrain = this.GetComponent<Terrain>();
+        terrainData = Terrain.activeTerrain.terrainData;
+
+        float[,] heightMap = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+
+        for(int width = 0; width < terrainData.heightmapResolution; width++){
+            for(int height = 0; height < terrainData.heightmapResolution; height++){
+                heightMap[width, height] = heightMapImage.GetPixel((int)(width * heightMapScale.x), (int)(height * heightMapScale.z)).grayscale * heightMapScale.y;
+            }
+        }
+
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    void FlattenTerrain(){
+        float[,] heightMap = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+
+        for(int width = 0; width < terrainData.heightmapResolution; width++){
+            for(int height = 0; height < terrainData.heightmapResolution; height++){
+                heightMap[width, height] = 0;
+            }
+        }
+
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    void OnValidate(){
+        if(flattenTerrainInEdit){
+            FlattenTerrain();
+        }
+        else if(loadHeightMap){
+            LoadHeightMapImage();
+        }
+    }
+
+    void OnDestroy(){
+        if(flattenTerrainOnExit){
+            FlattenTerrain();
+        }
+    }
+}
